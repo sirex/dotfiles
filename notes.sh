43f36db92e3b7b9e4b6b45aee2b1a327d08d5a38
Mantas <sirexas@gmail.com>
2024-02-13 08:21:50 +0200

    Migrate Alacritty config file from yaml to toml

---
+++ notes.sh
@@ -0,0 +1,9 @@
+# 2024-02-13 08:07
+
+cd dotfiles
+ln -fis $PWD/.config/alacritty/alacritty.toml ~/.config/alacritty
+y
+ls -l ~/.config/alacritty
+#| alacritty.toml -> ~/dotfiles/.config/alacritty/alacritty.toml
+
+
