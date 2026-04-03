7a364ef657169cddd0616cc840cab013bd1a546f
Mantas <sirexas@gmail.com>
2026-04-03 15:48:42 +0300

    neovim: center screen horizontally

--- nix/nixvim/keymaps.nix
+++ nix/nixvim/keymaps.nix
@@ -45,6 +45,7 @@ in
     { mode = "n"; key = "<A-j>"; action = "<c-d>"; options.desc = "Half page down"; }
     { mode = "n"; key = "<A-k>"; action = "<c-u>"; options.desc = "Half page up"; }
     { mode = "t"; key = "<A-k>"; action = "<C-\\><C-n><c-u>"; options.desc = "Half page up"; }
+    { mode = "n"; key = "zZ"; action = "zszH"; options.desc = "Center screen horizontally"; }
 
     # Terminal Mode Navigation
     { mode = "t"; key = "<Esc><Esc>"; action = "<C-\\><C-n>"; options.desc = "Exit terminal mode"; }
